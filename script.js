6d1cdf25f1af6203acb3b3bae7e43ab6dc108b64
danibabb2 <83610066+danibabb2@users.noreply.github.com>
2021-05-13 14:27:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
+    {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
-}
-        
+    var interest = principal * years * rate / 100;
+    var endYear = new Date().getFullYear()+parseInt(years);
+		
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
+    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>`
+    }
+    function updateRate() 
+    {
+    var rate_val = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rate_val;
+    }   
